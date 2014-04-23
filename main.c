3cda5face31df7c6eb58a45abd2fbaca82e61852
artprogramming <ranjunping@163.com>
2014-04-23 22:51:07 +0800

    add usb_irq.c for project and call usb_init in main()

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include "printf.h"
 
+extern void usb_isr(void) __interrupt 0;
+extern int  usb_init(void);
 extern void serial_init(void);
 
-int main(void)
+void main(void)
 {
 	serial_init();
+	usb_init();
 
 	printf("Welcome to the usb world!\n");
 
 	while (1);
-
-	return 0;
 }
