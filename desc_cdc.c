7470233e7af9ec17108c4f987144137666e58e32
artprogramming <ranjunping@163.com>
2014-05-15 14:15:22 +0800

    Create a new file desc_cdc.c

---
+++ desc_cdc.c
@@ -0,0 +1,183 @@
+/* Copy from book of Serial Port Complete at Table 16-3: Example descriptor for a full-speed CDC device */
+//#include <stdint.h>
+
+//////////////////////////////////////////////////////////////////////////
+//				        Device		                //
+//				     Configuration			//
+//		Interface(Communication)	Interface(Data)	        //
+//		Header Functional		Endpoint(Bulk IN)       //
+//	  Abstract Control Model Functional	Endpoint(Bulk OUT)      //
+//		Union Functional					//
+//	  Call Management Functional					//
+//		Endpoint(Interrupt IN)					//
+//////////////////////////////////////////////////////////////////////////
+
+unsigned char device_descriptor[] = {
+	0x12,		/* bLength */
+	0x01,		/* bDescriptorType */
+	0x00, 0x02,	/* bcdUSB */
+	0x02,		/* bDeviceClass */
+	0x00,		/* bDeviceSubClass */
+	0x00,		/* bDeviceProtocol */
+	0x40,		/* bMaxPacketSize0 */
+	0x25, 0x09,	/* idVendor */
+	0x50, 0x90,	/* idProduct */
+	0x00, 0x01,	/* bcdDevice */
+	0x01,		/* iManufacturer */
+	0x02,		/* iProduct */
+	0x03		/* iSerialNumber */
+	0x01		/* bNumConfigurations */
+}; 
+
+unsigned char config_descriptor[] = {
+	/* Configuration Descriptor */
+	0x09,		/* bLength */
+	0x02,		/* bDescriptorType */
+	0x43, 0x00,	/* wTotalLength */
+	0x02,		/* bNumInterfaces */
+	0x01,		/* bConfigurationValue */
+	0x00,		/* iConfiguration */
+	0x00,		/* bmAttributes */
+	0x32,		/* bMaxPower */
+	
+	/* Interface Descriptor */
+	0x09,		/* bLength */
+	0x04,		/* bDescriptorType */
+	0x00,		/* bInterfaceNumber */
+	0x00,		/* bAlternateSetting */
+	0x01,		/* bNumEndpoints */
+	0x02,		/* bInterfaceClass */
+	0x02,		/* bInterfaceSubClass */
+	0x01,		/* bInterfaceProtocol */
+	0x00,		/* iInterface */
+	
+	/* Header Function Descriptor */
+	0x05,		/* bFunctionLength */
+	0x24,		/* bDescriptorType */
+	0x00,		/* bDescriptorSubtype */
+	0x10, 0x01,	/* bcdCDC */
+	
+	/* Abstract Control Management Functional Descriptor */
+	0x04,		/* bFunctionLength */
+	0x24,		/* bDescriptorType */
+	0x02,		/* bDescriptorSubtype */
+	0x02,		/* bmCapabilities */
+	
+	/* Union Functional Descriptor */
+	0x05,		/* bFunctionLength */
+	0x24,		/* bDescriptorType */
+	0x06,		/* bDescriptorSubtype */
+	0x00,		/* bMasterInterface */
+	0x01,		/* bSlaveInterface0 */
+	
+	/* Call Management Functional Descriptor */
+	0x05,		/* bFunctionLength */
+	0x24,		/* bDescriptorType */
+	0x01,		/* bDescriptorSubtype */
+	0x00,		/* bmCapabilities */
+	0x01,		/* dDataInterface */
+	
+	/* Endpoint Descriptor */
+	0x07,		/* bLength */
+	0x05,		/* bDescriptorType */
+	0x82,		/* bEndpointAddress */
+	0x03,		/* bmAttributes */
+	0x08, 0x00,	/* wMaxPacketSize */
+	0x02,		/* bInterval */
+	
+	/* Interface Descriptor */
+	0x09,		/* bLength */
+	0x04,		/* bDescriptorType */
+	0x01,		/* bInterfaceNumber */
+	0x00,		/* bAlternateSetting */
+	0x02,		/* bNumEndpoints */
+	0x0A,		/* bInterfaceClass */
+	0x00,		/* bInterfaceSubClass */
+	0x00,		/* bInterfaceProtocol */
+	0x00,		/* iInterface */
+	
+	/* Endpoint Descriptor */
+	0x07,		/* bLength */
+	0x05,		/* bDescriptorType */
+	0x81,		/* bEndpointAddress */
+	0x02,		/* bmAttributes */
+	0x40, 0x00,	/* wMaxPacketSize */
+	0x00,		/* bInterval */
+	
+	/* Endpoint Descriptor */
+	0x07,		/* bLength */
+	0x05,		/* bDescriptorType */
+	0x01,		/* bEndpointAddress */
+	0x02,		/* bmAttributes */
+	0x40, 0x00,	/* wMaxPacketSize */
+	0x00,		/* bInterval */
+};
+
+unsigned char language_id[] = {
+	0x04,		/* bLength */
+	0x03,		/* bDescriptorType */
+	0x09, 0x04,	/* wLANGID */
+};
+
+unsigned char manufacturer_string[] = {
+	0x2C,		/* bLength */
+	0x03,		/* bDescriptorType */
+	'L', 0x00,
+	'a', 0x00,
+	'k', 0x00,
+	'e', 0x00,
+	'v', 0x00,
+	'i', 0x00,
+	'e', 0x00,
+	'w', 0x00,
+	' ', 0x00,
+	'R', 0x00,
+	'e', 0x00,
+	's', 0x00,
+	'e', 0x00,
+	'a', 0x00,
+	'r', 0x00,
+	'c', 0x00,
+	'h', 0x00,
+	' ', 0x00,
+	'L', 0x00,
+	'L', 0x00,
+	'C', 0x00,
+};
+
+unsigned char product_string[]= {
+	0x20,		/* bLength */
+	0x03,		/* bDescriptorType */
+	'C', 0x00,
+	'O', 0x00,
+	'M', 0x00,
+	' ', 0x00,
+	'P', 0x00,
+	'o', 0x00,
+	'r', 0x00,
+	't', 0x00,
+	' ', 0x00,
+	'D', 0x00,
+	'e', 0x00,
+	'v', 0x00,
+	'i', 0x00,
+	'c', 0x00,
+	'e', 0x00,
+};
+
+unsigned char serial_number_string[] = {
+	0x1A,		/* bLength */
+	0x03,		/* bDescriptorType */
+	'1', 0x00,
+	'2', 0x00,
+	'3', 0x00,
+	'4', 0x00,
+	'5', 0x00,
+	'6', 0x00,
+	'7', 0x00,
+	'8', 0x00,
+	'9', 0x00,
+	'A', 0x00,
+	'B', 0x00,
+	'C', 0x00,
+};
