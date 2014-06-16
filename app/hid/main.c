e317e9fcc73f431587c51f0afa27ec697189b6cd
artprogramming <ranjunping@163.com>
2014-06-16 23:27:48 +0800

    report key to host

--- app/hid/main.c
+++ app/hid/main.c
@@ -1,7 +1,13 @@
+#include <at89x52.h>
+
+#include "key.h"
+
 extern void usb_irq(void) __interrupt 0;
 extern int  usb_init(void);
 extern void hid_init(void);
 extern void serial_init(void);
+
+extern int  reportkey(uint8_t key);
 
 void main(void)
 {
@@ -9,5 +15,7 @@ void main(void)
 	hid_init();
 	usb_init();
 
-	while (1);
+	while (1) {
+		reportkey(keyscan());
+	}
 }
