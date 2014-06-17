eef216cb52abc1263a5796feaa5359913c67a3be
artprogramming <ranjunping@163.com>
2014-06-17 11:34:21 +0800

    export usb_set_device_state() and usb_get_device_state()

--- core/usb.h
+++ core/usb.h
@@ -2,5 +2,8 @@
 #define __USB_H
 
 extern void usb_send_zero_length_packet(void);
+extern void usb_set_device_state(enum usb_device_state new_state);
+extern enum usb_device_state usb_get_device_state(void);
 extern void usb_register_class_driver(void (*handler)(struct setup_packet *setup));
+
 #endif /* __USB_H */
